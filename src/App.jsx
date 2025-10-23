d5dacb50a90496cce591d7f7df1e7da06649af73
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-10-23 18:16:59 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,33 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
-const Card=({title}) => {
-  const [hasLiked,setHasLiked] = useState(false);
-  return(
-    <div className='card'>
-      <img src="https://imgs.search.brave.com/jlzE0FpISbjTFo1CDP3d_soiwgBZiuzi0cHOKfn3o2U/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZ2V0aW1nLmFp/L21lZGlhL2dldGlt/Z19haV9pbWctdExa/UUwzS1UyeE9nRDNO/c1hNZVp1LmpwZWc" alt="" />
-      <h2>
-        {title}
-      </h2>
-      <button onClick={()=> setHasLiked(true)}>
-        {hasLiked?'❤️' : "🤍"}
-          
+const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  return (
+    <div className="card">
+      <img
+        src="https://imgs.search.brave.com/jlzE0FpISbjTFo1CDP3d_soiwgBZiuzi0cHOKfn3o2U/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZ2V0aW1nLmFp/L21lZGlhL2dldGlt/Z19haV9pbWctdExa/UUwzS1UyeE9nRDNO/c1hNZVp1LmpwZWc"
+        alt=""
+      />
+      <h2>{title}</h2>
+      <button onClick={() => setHasLiked(true)}>
+        {hasLiked ? "❤️" : "🤍"}
       </button>
-       
     </div>
-  )
-}
- const App=() =>{
- 
-   
-  return(
+  );
+};
+const App = () => {
+  return (
     // react fragment <>
-    <div className='card-container'>
-      
-    <Card title="The Lion King" />
-    <Card title="Avatar"/>
-    <Card title="Star Wars" />
-    <Card title="Titanic" />
-    <Card title="# Idiots"/>
+    <div className="card-container">
+      <Card title="The Lion King" />
+      <Card title="Avatar" />
+      <Card title="Star Wars" />
+      <Card title="Titanic" />
+      <Card title="3 Idiots" />
     </div>
-  )
- }
+  );
+};
 
-export default App
- 
+export default App;
