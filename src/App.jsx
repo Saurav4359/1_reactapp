692d57ef417fcec6c602e94b8305df01bfef71ea
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-10-25 00:11:16 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       <Card title="The Lion King" />
       <Card title="Avatar" imgg="https://imgs.search.brave.com/-IQvvzpizYQw19c9uawjKVCIIbQ1H6aaSrkuKy7gUMk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9sdW1p/ZXJlLWEuYWthbWFp/aGQubmV0L3YxL2lt/YWdlcy9wX2Rpc25l/eXBsdXNvcmlnaW5h/bHNfYXZhdGFyX3Ro/ZXdheW9md2F0ZXJf/cG9zdGVyX3JlYnJh/X2ZhMDg2MzZkLmpw/ZWc"/>
       <Card title="Star Wars" imgg="https://imgs.search.brave.com/Nn6aVxla9nnMbaAw1enz-w1azNcdhaekLBSIOmgrm4s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hcnRv/ZnRoZW1vdmllcy5j/by51ay9jZG4vc2hv/cC9wcm9kdWN0cy9J/TUdfODI3Nl83MDA4/MzRlMy0yMGZlLTQz/NzEtYWI1ZS0xZTI2/YTExYWNlZmVfMzQ1/eDM0NUAyeC5qcGc_/dj0xNjUxMDg0ODQ3"/>
-      <Card title="Titanic" />
+      <Card title="Titanic" imgg="https://imgs.search.brave.com/0MyttxdNC0YwgN2tGO6rYFQvuaYdKqYKdgBrm4PMZRc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS1jYWNoZS5jaW5l/bWF0ZXJpYWwuY29t/L3AvNTAweC9wd3p4/emluZS90aXRhbmlj/LW1vdmllLWNvdmVy/LmpwZz92PTE0NTY2/Mjc2NTY"/>
       <Card title="3 Idiots" />
     </div>
   );
