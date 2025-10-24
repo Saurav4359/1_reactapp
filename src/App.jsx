1f53d32d9a8e1f5fa99bedb376d5b0d13ca14da5
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-10-24 19:51:18 +0530

    img update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from "react";
 
 import "./App.css";
 
-const Card = ({ title }) => {
+const Card = ({ title ,imgg}) => {
   const [hasLiked, setHasLiked] = useState(false);
   return (
     <div className="card">
       <img
-        src="https://imgs.search.brave.com/jlzE0FpISbjTFo1CDP3d_soiwgBZiuzi0cHOKfn3o2U/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZ2V0aW1nLmFp/L21lZGlhL2dldGlt/Z19haV9pbWctdExa/UUwzS1UyeE9nRDNO/c1hNZVp1LmpwZWc"
+        src= {imgg} 
         alt=""
       />
       <h2>{title}</h2>
-      <button onClick={() => setHasLiked(true)}>
+      <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? "❤️" : "🤍"}
       </button>
     </div>
@@ -22,7 +22,7 @@ const App = () => {
     // react fragment <>
     <div className="card-container">
       <Card title="The Lion King" />
-      <Card title="Avatar" />
+      <Card title="Avatar" imgg="https://imgs.search.brave.com/-IQvvzpizYQw19c9uawjKVCIIbQ1H6aaSrkuKy7gUMk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9sdW1p/ZXJlLWEuYWthbWFp/aGQubmV0L3YxL2lt/YWdlcy9wX2Rpc25l/eXBsdXNvcmlnaW5h/bHNfYXZhdGFyX3Ro/ZXdheW9md2F0ZXJf/cG9zdGVyX3JlYnJh/X2ZhMDg2MzZkLmpw/ZWc"/>
       <Card title="Star Wars" />
       <Card title="Titanic" />
       <Card title="3 Idiots" />
